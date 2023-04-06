8c88488762d0948a275bf0a18509b280172e0bd5
teem001 <107809346+teem001@users.noreply.github.com>
2023-04-06 22:23:26 +0100

    the cicd

---
+++ src/main/java/com/fundallassessment/app/FundallAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.fundallassessment.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundallAssessmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FundallAssessmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fundallassessment/app/FundallAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fundallassessment.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundallAssessmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
